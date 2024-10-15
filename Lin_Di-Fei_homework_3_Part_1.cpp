b0f801b415b95919a5d2bae61ea18951ceaa3945
DiFeiLin <difeil2@nycstudents.net>
2024-10-15 17:32:22 -0400

    w

--- Lin_Di-Fei_homework_3_Part_1.cpp
+++ Lin_Di-Fei_homework_3_Part_1.cpp
@@ -1,6 +1,6 @@
 /*Di Fei Lin
-10/7/24
-Lab 5 */
+10/15/24
+homework 3  */
 #include <iostream>
 #include <string>
 #include <iomanip>
@@ -11,44 +11,39 @@ int main(){
     string typeTransaction; // identifies the variables 
     double transactionMoney;
     double moneyTotal;
-    int amountTransaction = 7;
     double credit = 0;//credit amount starts at 0
     double debit = 0;//debit amount startsat 0
 
     ifstream myInput;//identify the input and output file 
     ofstream myOutput;
     
-    string WorD;
+    string WorD;//withdrawl or deposit 
 
     myInput.open("transaction.txt");//open the file 
 
     myInput>>moneyTotal;
 
-    while (amountTransaction>-1){ // it ends when the numbers of transactions hits 0
+   for (int i = 0; i < 7; i++){ // it ends when the numbers of transactions hits 0
 
     myInput>>WorD>>transactionMoney;
 
     if (WorD == "D"){ // if debit subtraction 
 
-        cout<<fixed<<showpoint;
+        cout<<fixed<<showpoint;//set to two decimal points 
         cout<<setprecision(2);
-        cout<<"Deposit: "<<transactionMoney<<endl;
-        cout<<"New Account balance: $" <<moneyTotal-transactionMoney<<endl;
-        moneyTotal = moneyTotal-transactionMoney;
-
-        amountTransaction = amountTransaction - 1;//deducts one from the amount of transactions left 
+        cout<<"Deposit: "<<transactionMoney<<endl;//prints deposit 
+        cout<<"New Account balance: $" <<moneyTotal+transactionMoney<<endl;///prints new amount 
+        moneyTotal = moneyTotal+transactionMoney;//add to ending balance 
         debit = debit + transactionMoney;// totals the debit deduction amount 
     }
 
     else {
 
-        cout<<fixed<<showpoint;
+        cout<<fixed<<showpoint;//set to two decimal points 
         cout<<setprecision(2);
-        cout<<"Withdrawl: "<<transactionMoney<<endl;
-        cout<<"New Account balance: $" <<moneyTotal+transactionMoney<<endl;// if credit addition 
-        moneyTotal = moneyTotal+transactionMoney;//adds to the amount money left 
-
-        amountTransaction = amountTransaction - 1;//deducts one from te amount fo transactions left 
+        cout<<"Withdrawl: "<<transactionMoney<<endl;//print withdrawl amount 
+        cout<<"New Account balance: $" <<moneyTotal-transactionMoney<<endl;//prints new amount 
+        moneyTotal = moneyTotal-transactionMoney;//adds to the amount money left 
         credit = credit+transactionMoney;//totals the debit deduction amount 
     }
 
@@ -61,20 +56,18 @@ int main(){
 
 }
 /*Withdrawl: 200.00
-New Account balance: $5200.00
+New Account balance: $4800.00
 Withdrawl: 800.00
-New Account balance: $6000.00
+New Account balance: $4000.00
 Withdrawl: 40.00
-New Account balance: $6040.00
+New Account balance: $3960.00
 Deposit: 90.00
-New Account balance: $5950.00
+New Account balance: $4050.00
 Deposit: 100.00
-New Account balance: $5850.00
+New Account balance: $4150.00
 Deposit: 1000.00
-New Account balance: $4850.00
+New Account balance: $5150.00
 Withdrawl: 20.00
-New Account balance: $4870.00
-Withdrawl: 20.00
-New Account balance: $4890.00
-Total credit addition: $1080.00
+New Account balance: $5130.00
+Total credit addition: $1060.00
 Total debit deduction: $1190.00*/
--- Lin_Di_Fei_homework_3_Part_2.cpp
+++ Lin_Di_Fei_homework_3_Part_2.cpp
@@ -1,6 +1,6 @@
 /*Di Fei Lin
-10/7/24
-Lab 5 */
+10/15/24
+homework 3  */
 #include <iostream>
 #include <string>
 #include <iomanip>
@@ -8,65 +8,72 @@ Lab 5 */
 using namespace std;
 
 int main(){
-    string typeTransaction; // identifies the variables 
+    string typeTransaction; // identifies the variables
+    
     double transactionMoney;
-    double moneyTotal = 5000;
+    double moneyTotal;
     double credit = 0;//credit amount starts at 0
     double debit = 0;//debit amount startsat 0
 
+    int TotalD = 0; // total number of deposits 
+    int TotalW = 0; // total number of withrdrawl
+
     ifstream myInput;//identify the input and output file 
     ofstream myOutput;
-    string WorD;
+    string WorD; // withdrawl or deposit
 
     myInput.open("transaction.txt");//open the file 
-    myOutput.open("bankstatment.txt");
-    myInput>>moneyTotal;
-    myOutput<<setfill('.');
+    myOutput.open("bankstatment.txt");//open the bankstatement
+    myInput>>moneyTotal; // money total is 5000
 
-    myOutput<<"TYPE"<<setw(12)<<"AMOUNT"<<setw(28)<<"BALANCE"<<endl;
+    myOutput<<setfill('.');//set fill for the statement 
+    myOutput<<"STARTING BALANCE: $ 5000.00"<<endl;//starting balance 
+    myOutput<<"TYPE"<<setw(12)<<"AMOUNT"<<setw(28)<<"BALANCE"<<endl;//the format 
     while (!myInput.eof()){ // it ends when the numbers of transactions hits 0
 
-    myInput>>WorD>>transactionMoney;
+    myInput>>WorD>>transactionMoney; //reads the input 
 
     if (WorD == "D"){ // if debit subtraction 
-        myOutput<<fixed<<showpoint;
+
+        myOutput<<fixed<<showpoint;//sets to two decimal points 
         myOutput<<setprecision(2);
-        myOutput<<"D"<<setw(14)<<transactionMoney<<setw(29)<<moneyTotal+transactionMoney<<endl;
-        moneyTotal = moneyTotal-transactionMoney;
+        myOutput<<"D"<<setw(14)<<transactionMoney<<setw(29)<<moneyTotal+transactionMoney<<endl;//prints the deposit statement 
+        moneyTotal = moneyTotal+transactionMoney;//update the moneytotal 
         debit = debit + transactionMoney;// totals the debit deduction amount 
+        TotalD = TotalD+1;//add one to total deposit 
+
     }
 
     else {
-        myOutput<<fixed<<showpoint;
+
+        myOutput<<fixed<<showpoint;//set to two decimal points
         myOutput<<setprecision(2);
-        myOutput<<"W"<<setw(14)<<transactionMoney<<setw(29)<<moneyTotal-transactionMoney<<endl;
-        moneyTotal = moneyTotal+transactionMoney;//adds to the amount money left 
-
-        credit = credit+transactionMoney;//totals the debit deduction amount 
+        myOutput<<"W"<<setw(14)<<transactionMoney<<setw(29)<<moneyTotal-transactionMoney<<endl;//prints the withdrawl statement 
+        moneyTotal = moneyTotal-transactionMoney;//adds to the amount money left 
+        credit = credit+transactionMoney;//totals the credit  deduction amount 
+        TotalW = TotalW+1;//add one to total withdrawl 
     }
-    }
 
+    }
+    myOutput<<"Ending Balance: $"<<moneyTotal<<endl;//prints ending balance 
     myOutput<<"Total credit addition: $"<<credit<<endl;//prints total credit amount 
     myOutput<<"Total debit deduction: $"<<debit<<endl;//prints total debit deductions 
-
+    myOutput<<"Amount of credit transactions: "<<TotalD<<endl;//prints total credit amount 
+    myOutput<<"Amount of debit transactions: "<<TotalW<<endl;//prints total debit deductions 
     return 0;
 
 }
-/*Withdrawl: 200.00
-New Account balance: $5200.00
-Withdrawl: 800.00
-New Account balance: $6000.00
-Withdrawl: 40.00
-New Account balance: $6040.00
-Deposit: 90.00
-New Account balance: $5950.00
-Deposit: 100.00
-New Account balance: $5850.00
-Deposit: 1000.00
-New Account balance: $4850.00
-Withdrawl: 20.00
-New Account balance: $4870.00
-Withdrawl: 20.00
-New Account balance: $4890.00
-Total credit addition: $1080.00
-Total debit deduction: $1190.00*/
+/*STARTING BALANCE: $ 5000.00
+TYPE......AMOUNT.....................BALANCE
+W........200.00......................4800.00
+W........800.00......................4000.00
+W.........40.00......................3960.00
+D.........90.00......................4050.00
+D........100.00......................4150.00
+D.......1000.00......................5150.00
+W.........20.00......................5130.00
+Ending Balance: $5130.00Total credit addition: $1060.00
+Total debit deduction: $1190.00
+Amount of credit transactions: 3
+Amount of debit transactions: 4
+*/
